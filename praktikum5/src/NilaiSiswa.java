f3f5c5acb1831e019cc53b578499058c52c3b64e
Rizal Fadlullah <rizal.fadlullah@gmail.com>
2021-04-08 09:19:35 +0700

    Solve all problems

--- praktikum5/src/NilaiSiswa.java
+++ praktikum5/src/NilaiSiswa.java
@@ -1,4 +1,9 @@
 import java.util.ArrayList;
+import java.util.Scanner;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 class siswa {
     private String nim;
     private String nama;
@@ -36,26 +41,54 @@ class siswa {
     }
 }
 
-public class NilaiSiswa {
-   
+public class NilaiSiswa extends JFrame {
+    public NilaiSiswa() {
+        setTitle("Daftar Nilai Siswa");
+        setSize(800, 600);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        userInterfaceGUI();
+        setVisible(true);
+    }
 
-    public static void main(String[] args) {
-       
+    private void userInterfaceGUI() {
+        DefaultTableModel model = new DefaultTableModel();
+        model.setColumnIdentifiers(new Object[]{
+            "NIM", "NAMA", "TEORI", "PRAKTEK"
+        });
+        JTable tabel = new JTable(model);
+
+        Scanner sc = new Scanner(System.in);
         ArrayList<siswa> tabsiswa = new ArrayList<>();
-        for (int counter = 0; counter < 10; counter++){
-            siswa s = new siswa("lkajdf","Rizal", counter,100);
+        System.out.print("Masukkan Jumlah Data\t: ");
+        int i = sc.nextInt();
+        for (int counter = 0; counter < i; counter++){
+            System.out.print("NIM\t:");
+            String nim = sc.next();
+            System.out.print("Nama\t:");
+            String nama = sc.next();
+            System.out.print("Teori\t:");
+            int teori = sc.nextInt();
+            System.out.print("Praktek\t:");
+            int praktek = sc.nextInt();
+            System.out.println("--------------------------");
+            siswa s = new siswa(nim, nama, teori, praktek);
             tabsiswa.add(s);
         }
         for(siswa s:tabsiswa){
-            System.out.println(s.getTeori());
+            Object[] dataRow = new Object[4];
+            dataRow[0] = s.getNim();
+            dataRow[1] = s.getNama();
+            dataRow[2] = s.getTeori();
+            dataRow[3] = s.getPraktek();
+            model.addRow(dataRow);
         };
-        // for (int counter =0; counter < tabsiswa.size(); counter++) {
-        //     System.out.println("NIM\t:"+tabsiswa.get(counter).getNim());
-            // System.out.println("Nama\t:"+tabsiswa.get(counter).getNama());
-        //     System.out.println("Teori\t:"+tabsiswa.get(counter).getTeori());
-        //     System.out.println("Praktek\t:"+tabsiswa.get(counter).getPraktek());
-        //     System.out.println("----------------------------------------------");
-        // }
-      
+        JScrollPane scroll = new JScrollPane();
+        scroll.setViewportView(tabel);
+        
+        getContentPane().add(scroll);
+    }
+    public static void main(String[] args) {
+        NilaiSiswa obj = new NilaiSiswa();
     }
 }
